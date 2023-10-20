a8ef5910680c1d770202308b9946582afdb2ec68
Nick <nikmaunt@gmail.com>
2023-10-20 17:16:25 +0400

    feat: prettier & eslint setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {Test1Component} from "./test1/test1.component";
+import { Test2Component } from './test2/test2.component';
+import { Test3Component } from './test3/test3.component';
+import { Test4Component } from './user/components/test4/test4.component';
+import { Test5Component } from './user/components/test5/test5.component';
+import { Test7Component } from './test7/test7.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Test1Component,
+    Test2Component,
+    Test3Component,
+    Test4Component,
+    Test5Component,
+    Test7Component
   ],
   imports: [
     BrowserModule,
@@ -15,4 +27,4 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class  AppModule { }
